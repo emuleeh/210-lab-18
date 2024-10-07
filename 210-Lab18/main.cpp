87ee9ccd14173e55150e09812f10a57cccd19e81
Emily <emuleehw@gmail.com>
2024-10-06 21:11:50 -0700

    Initial Commit

---
+++ 210-Lab18/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  210-Lab18
+//
+//  Created by E on 10/6/24.
+//  Copyright © 2024 E. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
